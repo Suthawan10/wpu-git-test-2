007a3521c44eb22f12412ec28b740fa7586ff97c
Suthawan10 <imadesuthawan@gmail.com>
2024-01-13 11:26:15 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, consectetur.</p>
+    <ul>
+        <li>Sutawan</li>
+    </ul>
 </body>
 </html>
